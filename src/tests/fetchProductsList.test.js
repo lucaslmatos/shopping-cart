5b7d019492e7e3a1d65879d76886d643aa756d73
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-16 16:19:28 -0300

    Ultimos testes implementados - Req 01

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -15,9 +15,19 @@ describe('Teste a função fetchProductsList', () => {
     expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+  it('fetch é chamado com o end point correto ao executar fetchProductsList', () => {
     fetchProductsList(arg);
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('A função FetchProductsList chamada com o argumento computador, deve retornar uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const expected = await fetchProductsList('computador');
+    expect(expected).toStrictEqual(computadorSearch);
+  });
+
+  it('A função FetchProductsList chamada sem argumento, deve retornar um erro com a mensagem:Termo de busca não informado',  async () => {
+    const expected2 = await fetchProductsList();
+    expect(expected2).toThrow(new Error('Termo de busca não informado'));
   });
 
   // it('...', () => {
