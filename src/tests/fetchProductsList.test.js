8c92881a7117b8af07ee6b834ad2149e632c60cc
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-16 15:50:03 -0300

    Primeiros testes implementados - Req 01

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -3,17 +3,21 @@ import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
+const arg = 'computador';
+
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList ).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    fetchProductsList(arg);
+    expect(fetch).toHaveBeenCalled();
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    fetchProductsList(arg);
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
   // it('...', () => {
