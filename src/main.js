59cd5a4fda3a62e7090b7422be9c8fa01b6f0d43
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-16 18:41:51 -0300

    Implementando os elementos na página - Req 03

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 // Início.
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const mercadorias = await fetchProductsList('computador');
+
+for (let i = 0; i < mercadorias.length; i += 1) {
+  const productsPlace = document.querySelector('.products');
+  const ref = mercadorias[i];
+  const { id, title, thumbnail, price } = ref;
+  productsPlace.appendChild(createProductElement({ id, title, thumbnail, price }));
+}
