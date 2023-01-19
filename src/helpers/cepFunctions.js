ce0b1833798f69dbf6f2247f27dde8b079e2590f
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-19 20:23:44 -0300

    Função para buscar o CEP digitado implementada - Req 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,18 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (cep) => {
+  const cepPlace = document.querySelector('.cart__address');
+  cepPlace.innerText = '';
+  try {
+    const promise1 = await (await fetch(`https://cep.awesomeapi.com.br/json/${cep}`)).json();
+    const promise2 = await (await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`)).json();
+    const p = await Promise.any([promise1, promise2]);
+    cepPlace.innerText = `${p.address_type} ${p.address_name} - ${
+      p.district} - ${p.city} - ${p.state}`;
+  } catch {
+    cepPlace.innerText = 'CEP não encontrado';
+  }
 };
 
 export const searchCep = () => {
-  // seu código aqui
+  const cepValue = document.querySelector('.cep-input');
+  getAddress(cepValue.value);
 };
