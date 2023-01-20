246acfef53a03605e9175325f0256afa0990ffe6
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-19 22:25:55 -0300

    Corrigindo bug - Req 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -4,9 +4,10 @@ export const getAddress = async (cep) => {
   try {
     const promise1 = await (await fetch(`https://cep.awesomeapi.com.br/json/${cep}`)).json();
     const promise2 = await (await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`)).json();
-    const p = await Promise.any([promise1, promise2]);
-    cepPlace.innerText = `${p.address_type} ${p.address_name} - ${
-      p.district} - ${p.city} - ${p.state}`;
+    const cepInfo = await Promise.any([promise1, promise2]);
+    cepPlace.innerText = `${cepInfo.address_type} ${
+      cepInfo.address_name} - ${cepInfo.district} - ${
+      cepInfo.city} - ${cepInfo.state}`;
   } catch {
     cepPlace.innerText = 'CEP não encontrado';
   }
